3c528cc41a3706b852aabd131c8ce51019339721
Karlo Belavic <lahur.123@gmail.com>
2021-07-03 20:06:19 +0200

    enviroment fix

--- banking-angular/src/environments/environment.prod.ts
+++ banking-angular/src/environments/environment.prod.ts
@@ -1,17 +1,5 @@
-const keycloakConfig = {
-  url: 'http://belavic-accounting/auth',
-  realm: 'accounting',
-  clientId: 'angular-app',
-  redirectUri: window.location.origin,
-  responseType: 'code',
-  scope: 'openid profile email',
-  requireHttps: false,
-  showDebugInformation: true,
-  disableAtHashCheck: true
-};
-
 export const environment = {
   production: true,
-  keycloak: keycloakConfig,
-  url: 'http://localhost:8082/api'
+  apiUrl: window['env'].apiUrl || 'default',
+  debug: window['env'].debug || false
 };
